b02eea24923cfaf2c5c3c6ca4333751d37d9e077
Vishwanathan Dharmalingam <vishwanathan@tvisoft.in>
2023-09-09 12:36:48 +0530

    corrections

--- components/HighlightItem.tsx
+++ components/HighlightItem.tsx
@@ -14,59 +14,64 @@ const HighlightItem = ({ item }) => {
   const ref = useRef();
   const navigation = useNavigation();
   const { colorScheme } = useColorScheme();
-
+  console.log("HighlightItem", item);
+  // console.log('HighlightItem',item?.value)
+  // console.log('HighlightItem',item?.value)
+  const isItems = item?.value?.length > 0;
   return (
-    <ViewShot ref={ref}>
-      <StyledView
-        className={`bg-white mb-5 p-2 dark:bg-[${colors.darkColors.body}]`}
-        style={{
-          backgroundColor:
-            colorScheme === "dark" ? colors.darkColors.body : "white",
-        }}
-      >
-        <Text
-          className="self-center"
+    isItems ? (
+      <ViewShot ref={ref}>
+        <StyledView
+          className={`bg-white mb-5 p-2 dark:bg-[${colors.darkColors.body}]`}
           style={{
-            paddingVertical: 4,
-            paddingHorizontal: 12,
             backgroundColor:
-              colorScheme === "dark" ? "#FFA1A5" : "rgba(200, 33, 40, 0.1)",
-            color: colorScheme === "dark" ? colors.white : colors.black,
-            borderRadius: 29,
+              colorScheme === "dark" ? colors.darkColors.body : "white",
           }}
         >
-          {item?.date}
-        </Text>
-        {item?.value?.map(({ highlight, id }) => {
-          return (
-            <PressableOpacity
-              onPress={() => {
-                navigation.navigate(ROUTES.NEWSFEED_ID, { id });
-              }}
-            >
-              <Text
-                className={`text-black dark:text-[${colors.darkColors.text}]`}
-                style={{
-                  lineHeight: 18,
-                  marginTop: 16,
+          <Text
+            className="self-center"
+            style={{
+              paddingVertical: 4,
+              paddingHorizontal: 12,
+              backgroundColor:
+                colorScheme === "dark" ? "#FFA1A5" : "rgba(200, 33, 40, 0.1)",
+              color: colorScheme === "dark" ? colors.white : colors.black,
+              borderRadius: 29,
+            }}
+          >
+            {item?.date}
+          </Text>
+          {item?.value?.map(({ highlight, id }) => {
+            return (
+              <PressableOpacity
+                onPress={() => {
+                  navigation.navigate(ROUTES.NEWSFEED_ID, { id });
                 }}
-                key={id}
               >
-                {highlight}
-              </Text>
-            </PressableOpacity>
-          );
-        })}
-        <View className="self-end" style={{ width: 60 }}>
-          <ShareIcon
-            viewref={ref}
-            isBookmarked={undefined}
-            addToBookMark={undefined}
-            news={undefined}
-          ></ShareIcon>
-        </View>
-      </StyledView>
-    </ViewShot>
+                <Text
+                  className={`text-black dark:text-[${colors.darkColors.text}]`}
+                  style={{
+                    lineHeight: 18,
+                    marginTop: 16,
+                  }}
+                  key={id}
+                >
+                  {highlight}
+                </Text>
+              </PressableOpacity>
+            );
+          })}
+          <View className="self-end" style={{ width: 60 }}>
+            <ShareIcon
+              viewref={ref}
+              isBookmarked={undefined}
+              addToBookMark={undefined}
+              news={undefined}
+            ></ShareIcon>
+          </View>
+        </StyledView>
+      </ViewShot>
+    ):null
   );
 };
 
